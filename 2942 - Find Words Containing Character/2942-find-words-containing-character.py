8aafc4070ca0a8bea39f70d7aa058960350dbd5b
AkseliManninen <akseli.manninen@gmail.com>
2024-10-08 19:34:57 +0300

    Solve 2942

---
+++ 2942 - Find Words Containing Character/2942-find-words-containing-character.py
@@ -0,0 +1,11 @@
+# Runtime 42 ms
+
+class Solution(object):
+    def findWordsContaining(self, words, x):
+        """
+        :type words: List[str]
+        :type x: str
+        :rtype: List[int]
+        """
+
+        return [i for i, w in enumerate(words) if x in w]

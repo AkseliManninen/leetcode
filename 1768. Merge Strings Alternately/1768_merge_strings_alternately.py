93f3ea7c53775fbbabb5afff6d1a27a4a1551de0
AkseliManninen <akseli.manninen@gmail.com>
2024-10-10 08:46:43 +0300

    Solve 1768

---
+++ 1768. Merge Strings Alternately/1768_merge_strings_alternately.py
@@ -0,0 +1,17 @@
+# Runtime 8 ms
+
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+
+        res = ""
+        min_len = min(len(word1), len(word2))
+
+        for i in range(min_len):
+            res += word1[i] + word2[i]
+        
+        return res + word1[min_len:] + word2[min_len:]

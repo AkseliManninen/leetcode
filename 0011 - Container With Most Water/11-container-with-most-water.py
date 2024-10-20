abc16f248e19ebeea719227aa31db9aa9779004e
AkseliManninen <akseli.manninen@gmail.com>
2024-10-20 09:05:11 +0300

    Solve 11

---
+++ 0011 - Container With Most Water/11-container-with-most-water.py
@@ -0,0 +1,21 @@
+# Runtime: 95 ms
+
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+
+        left = 0
+        right = len(height)-1
+        
+        max_water = min(height[left], height[right]) * (right-left) # height x width
+
+        while left < right:
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+            max_water = max((min(height[left], height[right]) * (right-left)), max_water)
+        return max_water

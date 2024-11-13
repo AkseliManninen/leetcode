e20f50b7b0f9cd2a17e99cbb70c802fd044b20fb
AkseliManninen <akseli.manninen@gmail.com>
2024-11-13 21:46:52 +0200

    Solve 577

---
+++ 0577. Employee Bonus/577_employee_bonus.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
+    df = pd.merge(employee, bonus, on="empId", how="outer")
+    df = df[["name", "bonus"]]
+    return df[(df["bonus"] < 1000) | (df["bonus"].isna())]
+    

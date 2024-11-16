32c589978393d25d47d878d983a837b8d388d65e
AkseliManninen <akseli.manninen@gmail.com>
2024-11-16 07:52:05 +0200

    Solve 345

---
+++ 0345. Reverse Vowels of a String/345_reverse_vowels_of_a_string.py
@@ -0,0 +1,22 @@
+# Runtime 1644 ms --> This solution is not efficient.
+
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        vowels = ["a", "e", "i", "o", "u"]
+        
+        s_vowels = [c for c in s if c.lower() in vowels]
+
+        res = ""
+        for i in range(len(s)):
+            if s[i].lower() not in vowels:
+                res += s[i]
+            else:
+                res += s_vowels[-1]
+                s_vowels = s_vowels[:-1]
+        
+        return res

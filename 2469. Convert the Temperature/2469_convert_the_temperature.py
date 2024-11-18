649cd9802d0cd000714bce0f83444be13142911e
AkseliManninen <akseli.manninen@gmail.com>
2024-11-18 17:30:21 +0200

    Solve 2469

---
+++ 2469. Convert the Temperature/2469_convert_the_temperature.py
@@ -0,0 +1,6 @@
+# Time complexity: O(1)
+# Space complexity: O(1)
+
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:  
+        return([celsius+273.15,celsius*1.8+32.00])

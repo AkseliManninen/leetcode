315fc812955eb2b396d17ff3a5f11ef04d7f4649
AkseliManninen <akseli.manninen@gmail.com>
2024-11-29 20:38:32 +0200

    Solve 2885

---
+++ 2885. Rename Columns/2885_rename_columns.py
@@ -0,0 +1,7 @@
+# Runtime 463 ms
+
+import pandas as pd
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    students = students.rename(columns={"id": "student_id", "first": "first_name", "last": "last_name", "age": "age_in_years"})
+    return students
